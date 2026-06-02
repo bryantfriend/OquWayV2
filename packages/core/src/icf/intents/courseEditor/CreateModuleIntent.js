e7e6a88a55628ee0837780a498daea9c63b781a0
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2026-06-02 14:09:19 +0600

    Add legacy module repair migration

--- packages/core/src/icf/intents/courseEditor/CreateModuleIntent.js
+++ packages/core/src/icf/intents/courseEditor/CreateModuleIntent.js
@@ -1,9 +1,9 @@
 import { validateAuthenticated, validateCourseId } from "../../stages/validate/validators.js";
 import { normalizeCourseId, normalizeModuleShell } from "../../stages/normalize/normalizers.js";
-import { attachActorContext, attachActorRoleContext } from "../../stages/addContext/contexts.js?v=1.1.26-buildcheck";
-import { attachCourseDocument, attachModulesCollection } from "../../stages/addContext/contexts.js?v=1.1.26-buildcheck";
+import { attachActorContext, attachActorRoleContext } from "../../stages/addContext/contexts.js?v=1.1.27-module-repair";
+import { attachCourseDocument, attachModulesCollection } from "../../stages/addContext/contexts.js?v=1.1.27-module-repair";
 import { requireCourseCreatorAuthorization } from "../../stages/authorize/authorizers.js";
-import { processCreateModule } from "../../stages/process/processors.js?v=1.1.26-buildcheck";
+import { processCreateModule } from "../../stages/process/processors.js?v=1.1.27-module-repair";
 import { emitIntentResult } from "../../stages/emit/emitters.js";
 
 export function CreateModuleIntent() {
