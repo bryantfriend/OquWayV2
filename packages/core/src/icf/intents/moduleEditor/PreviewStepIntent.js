5b0473c6495cad6e5bd53de97597d2e48e2ab7b7
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2026-06-02 13:19:10 +0600

    Add PreviewStepIntent

---
+++ packages/core/src/icf/intents/moduleEditor/PreviewStepIntent.js
@@ -0,0 +1,36 @@
+import { validateAuthenticated, validateCourseId, validateLearningModeId, validateModuleId, validatePracticeModeStepId } from "../../stages/validate/validators.js";
+import { normalizeCourseId } from "../../stages/normalize/normalizers.js";
+import { attachActorContext, attachActorRoleContext, attachStepPreviewContext } from "../../stages/addContext/contexts.js";
+import { requireCourseCreatorAuthorization } from "../../stages/authorize/authorizers.js";
+import { processPreviewStep } from "../../stages/process/processors.js";
+import { emitIntentResult } from "../../stages/emit/emitters.js";
+
+export function PreviewStepIntent() {
+  return {
+    type: "PreviewStepIntent",
+    validate: [
+      validateAuthenticated,
+      validateCourseId,
+      validateModuleId,
+      validateLearningModeId,
+      validatePracticeModeStepId
+    ],
+    normalize: [
+      normalizeCourseId
+    ],
+    addContext: [
+      attachActorContext,
+      attachActorRoleContext,
+      attachStepPreviewContext
+    ],
+    authorize: [
+      requireCourseCreatorAuthorization
+    ],
+    process: [
+      processPreviewStep
+    ],
+    emit: [
+      emitIntentResult
+    ]
+  };
+}
