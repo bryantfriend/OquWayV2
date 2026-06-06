85655bc6f272c65ff5e20a4c4584b1d5e595f25f
bryantfriend <72289778+bryantfriend@users.noreply.github.com>
2026-06-06 22:32:28 +0600

    Remove invalid Windows backslash paths

--- packages/icf\admin\index.js
+++
@@ -1,8 +0,0 @@
-export {
-  getIntent,
-  listIntents,
-  registerIntent,
-  registerIntentDefinitions,
-  runAdminIntent
-} from "./intentRegistry.js?v=1.1.82-shared-command-center-shell";
-export { runIntentPipeline } from "./runIntentPipeline.js?v=1.1.82-shared-command-center-shell";
--- packages/icf\index.js
+++
@@ -1,3 +0,0 @@
-export { createIntent } from "../core/src/icf/engine/createIntent.js?v=1.1.82-shared-command-center-shell";
-export { runIntentPipeline } from "../core/src/icf/engine/runIntentPipeline.js?v=1.1.82-shared-command-center-shell";
-export { getIntentDefinition } from "../core/src/icf/engine/intentRegistry.js?v=1.1.82-shared-command-center-shell";
